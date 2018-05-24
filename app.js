c82c22861242c7cfd3dfffbbdd6dc63195c6e769
David Eliason <david@davethemaker.com>
2018-05-24 09:38:07 -0700

    create app.js where express server is spun up, config, comment route

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var book = require('./routes/comments');
+var app = express();
+
+// MIDDLEWARE
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('/api/comment', comment);
+
+module.exports = app;
