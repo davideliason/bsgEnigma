419c3a16f08b82ba02f8e9634b155036d2286e1e
David Eliason <david@davethemaker.com>
2018-06-01 09:00:41 -0700

    add MIME type to vanilla http server output

--- server/vanilla_server.js
+++ server/vanilla_server.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 var server = http.createServer(function(req,res) {
-	res.writeHead(200);
-	res.end("hello world");
+	res.writeHead(200,{"Content-Type" : "text/html"});
+	res.end("<h3>BSG Enigma</h3>");
 });
 
 server.listen(8080);
