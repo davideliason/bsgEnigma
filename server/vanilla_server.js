00af41cfda2b34cb0ecd14f5d483474703fd2693
David Eliason <david@davethemaker.com>
2018-06-01 09:21:14 -0700

    use querystring to parse the query string from req body

--- server/vanilla_server.js
+++ server/vanilla_server.js
@@ -1,11 +1,16 @@
 var http = require('http');
 var url = require("url");
+var querystring = require('querystring');
 
 var server = http.createServer(function(req, res) {
+	var params = querystring.parse(url.parse(req.url).query);
 	var page = url.parse(req.url).pathname;
 	var all_req = url.parse(req.url);
+
 	console.log(page);
 	console.log(all_req);
+
+	console.log(params['firstname']);
 
 	res.writeHead(200, {"Content-Type": "text/html"});
 	if(page == '/'){
