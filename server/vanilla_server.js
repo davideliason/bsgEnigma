d5e9640061f8d77f01be571aa45d79a86e43aace
David Eliason <david@davethemaker.com>
2018-06-01 09:14:19 -0700

    parse http request body and pathname

--- server/vanilla_server.js
+++ server/vanilla_server.js
@@ -1,8 +1,17 @@
 var http = require('http');
+var url = require("url");
 
-var server = http.createServer(function(req,res) {
-	res.writeHead(200,{"Content-Type" : "text/html"});
-	res.end("<h3>BSG Enigma</h3>");
-});
+var server = http.createServer(function(req, res) {
+	var page = url.parse(req.url).pathname;
+	var all_req = url.parse(req.url);
+	console.log(page);
+	console.log(all_req);
+
+	res.writeHead(200, {"Content-Type": "text/html"});
+	res.write('hello world');
+	res.end();
+    });
+
 
 server.listen(8080);
+
